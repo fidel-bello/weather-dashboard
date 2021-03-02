0b156518f29afe38cb2ac726dc71fe6b2088b25f
fidel bello <59423@bears.smcc.edu>
2021-03-01 19:39:02 -0500

    added val method with empty string to clear search input

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const apiKey = "&appid=b4df29657122f208c7d04c1f03714e1a";
 let date = new Date()
 
 $('#searchBtn').on('click', function(){
+    //user input value
     searchcity = $('#searchCity').val();
+    // clear search
+    $('#searchCity').val("");
+    // full url
     const queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchcity + apiKey;
     $.ajax({
         url: queryUrl,
