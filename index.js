b3af5e58717f215901e67796ff609fcb641c0137
fidel bello <59423@bears.smcc.edu>
2021-03-01 19:23:49 -0500

    added ajax that works with my personal api key on click button

--- index.js
+++ index.js
@@ -3,4 +3,15 @@ let searchcity = $("#searchCity").val()
 
 //api key
 const apiKey = "&appid=b4df29657122f208c7d04c1f03714e1a";
+// current date
+let date = new Date()
 
+$('#searchBtn').on('click', function(){
+    const queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + "Madison" + apiKey;
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function(response){
+        console.log(response)
+    })
+})
